5f331d44093d86df62b025f746a9349b44cb884d
NiniDarsa <ninodarsa700@gmail.com>
2025-04-04 11:27:18 -0700

    style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,53 @@
-import GlobalStyle from "../Style/GlobalStyle"
-import styled from "styled-components"
+import GlobalStyle from "../Style/GlobalStyle";
+import styled from "styled-components";
 
-import Game1 from "./Game1"
-import Game2 from "./Game2"
-import { Link, Route, Routes } from "react-router-dom"
-import Game3 from "./Game3"
+import Game1 from "./Game1";
+import Game2 from "./Game2";
+import { Link, Route, Routes } from "react-router-dom";
+import Game3 from "./Game3";
 
 function App() {
   return (
     <StyledApp>
-       <GlobalStyle/>
+      <GlobalStyle />
       <Routes>
-       <Route path="/" element={<Game1/>}/>
-       <Route path="/game2" element={<Game2/>}/>
-       <Route path="/game3" element={<Game3/>}/>
+        <Route path="/" element={<Game1 />} />
+        <Route path="/game2" element={<Game2 />} />
+        <Route path="/game3" element={<Game3 />} />
       </Routes>
       <StyledBtnContainer>
-       <button ><Link to="/">Game N1</Link></button>
-       <button ><Link to="/game2">Game N2</Link></button>
-       <button ><Link to="/game3">Game N3</Link></button>
+        <button>
+          <Link to="/">Game N1</Link>
+        </button>
+        <button>
+          <Link to="/game2">Game N2</Link>
+        </button>
+        <button>
+          <Link to="/game3">Game N3</Link>
+        </button>
       </StyledBtnContainer>
     </StyledApp>
-  )
+  );
 }
 
-export default App
+export default App;
 
-const StyledApp=styled.div`
+const StyledApp = styled.div`
   height: 100vh;
   position: relative;
-`
-const StyledBtnContainer=styled.div`
-    position: absolute;
-    bottom: 2rem;
-    left:50%;
-    transform: translateX(-50%);
-    button{
-      padding: 1rem;
-      a{
-        text-decoration: none;
-        color: black;
-      }
+`;
+const StyledBtnContainer = styled.div`
+  position: absolute;
+  bottom: 2rem;
+  left: 50%;
+  transform: translateX(-50%);
+  button {
+    padding: 1rem;
+    a {
+      text-decoration: none;
+      color: black;
+      width: 100%;
+      height: 100%;
     }
-`
+  }
+`;
