804e57563d3256647e2ed598ea83ef56f58c980c
NiniDarsa <ninodarsa700@gmail.com>
2025-04-05 10:54:41 -0700

    game2 fix

--- src/CanvasMain.tsx
+++ src/CanvasMain.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react";
+import styled from "styled-components";
 
-const CanvasMain=()=>{
-const ref=useRef<HTMLCanvasElement>(null)
+const CanvasMain = () => {
+  const ref = useRef<HTMLCanvasElement>(null);
 
-
-const draw=(ctx:CanvasRenderingContext2D)=>{
+  const draw = (ctx: CanvasRenderingContext2D) => {
     ctx.beginPath();
     ctx.moveTo(20, 1000);
     ctx.lineTo(20, 20);
@@ -12,20 +12,39 @@ const draw=(ctx:CanvasRenderingContext2D)=>{
     ctx.lineTo(200, 80);
     ctx.strokeStyle = "white";
     ctx.stroke();
-}
+  };
 
-useEffect(()=>{
-    const canvas=ref.current
+  useEffect(() => {
+    const canvas = ref.current;
     if (canvas) {
-        const context = canvas.getContext('2d');
-        if (context) {
-            draw(context)
-        }
+      const context = canvas.getContext("2d");
+      if (context) {
+        draw(context);
       }
-   ;
-   
-},[draw])
+    }
+  }, [draw]);
 
-return <canvas ref={ref} width="500" height="340"  style={{position:"absolute", top:"1rem",left:"45.7rem"}}/>
-}
-export default CanvasMain
+  return <Canvas ref={ref} />;
+};
+export default CanvasMain;
+
+const Canvas = styled.canvas`
+  position: absolute;
+  top: 1rem;
+  left: 45.7rem;
+  width: 240px;
+  height: 340px;
+
+  /* Responsive styles */
+  @media (max-width: 768px) {
+    left: 25rem; /* Adjust the position */
+    width: 180px; /* Adjust canvas width */
+    height: 250px; /* Adjust canvas height */
+  }
+
+  @media (max-width: 480px) {
+    left: 15rem; /* Further adjust the position */
+    width: 150px; /* Adjust canvas width */
+    height: 210px; /* Adjust canvas height */
+  }
+`;
